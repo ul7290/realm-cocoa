fddc3bb4ddd5a69d744966695134276ef457f6d1
Ari Lazier <alazier@gmail.com>
2014-09-11 15:06:41 -0700

    tests

--- Realm/Tests/RealmTests.mm
+++ Realm/Tests/RealmTests.mm
@@ -298,12 +298,11 @@
     XCTAssertTrue(notificationFired);
 }
 
-
 - (void)testInMemoryRealm
 {
     RLMRealm *inMemoryRealm = [RLMRealm inMemoryRealmWithIdentifier:@"identifier"];
 
-    [self waitForNotification:RLMRealmRefreshRequiredNotification realm:inMemoryRealm block:^{
+    [self waitForNotification:RLMRealmDidChangeNotification realm:inMemoryRealm block:^{
         RLMRealm *inMemoryRealm = [RLMRealm inMemoryRealmWithIdentifier:@"identifier"];
         [inMemoryRealm beginWriteTransaction];
         [StringObject createInRealm:inMemoryRealm withObject:@[@"a"]];
@@ -321,7 +320,6 @@
 
     // make sure we can't open disk-realm at same path
     XCTAssertThrows([RLMRealm realmWithPath:anotherInMemoryRealm.path], @"Should throw");
-
 }
 
 - (void)testRealmFileAccess
